89afe42eb6618d83d42cb3e98f0186b2eaef28f6
Thanos-in <sayyodbee2006@gmail.com>
2025-10-10 16:29:53 +0500

    chore: update packages and modify router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 import LoginPage from '@/views/pages/LoginPage.vue'
 import WarpingPage from '@/views/pages/WarpingPage.vue'
 import WeavingPage from '@/views/pages/WeavingPage.vue'
@@ -7,24 +8,34 @@ import PackagePage from '@/views/pages/PackagePage.vue'
 import ExpenseHistory from '@/views/pages/ExpenseHistory.vue'
 import HistoryAction from '@/views/pages/HistoryAction.vue'
 import PrintLabel from '@/views/pages/PrintLabel.vue'
-import Slitting from '@/views/pages/Slitting.vue'
 
 const routes = [
-  { path: '/:pathMatch(.*)*', component: () => import('@/views/pages/maintenance/error/Error404Page.vue')},
+  { path: '/:pathMatch(.*)*', component: () => import('@/views/pages/maintenance/error/Error404Page.vue') },
   { path: '/', name: 'login', component: LoginPage },
-  { path: '/warping', name: 'Warping', component: WarpingPage },
-  { path: '/weaving', name: 'Weaving', component: WeavingPage },
-  { path: '/manualfix', name: 'ManualFix', component: ManualFixPage },
-  { path: '/package', name: 'Package', component: PackagePage },
-  { path: '/consumes', name: 'ExpenseHistory', component: ExpenseHistory },
-  { path: '/history', name: 'HistoryAction', component: HistoryAction },
-  { path: '/print-label', name: 'PrintLabel', component: PrintLabel },
-  { path: '/slitting', name: 'Slitting', component: Slitting }
+  { path: '/warping', name: 'Warping', component: WarpingPage, meta: { requiresAuth: true } },
+  { path: '/weaving', name: 'Weaving', component: WeavingPage, meta: { requiresAuth: true } },
+  { path: '/manualfix', name: 'ManualFix', component: ManualFixPage, meta: { requiresAuth: true } },
+  { path: '/package', name: 'Package', component: PackagePage, meta: { requiresAuth: true } },
+  { path: '/consumes', name: 'ExpenseHistory', component: ExpenseHistory, meta: { requiresAuth: true } },
+  { path: '/history', name: 'HistoryAction', component: HistoryAction, meta: { requiresAuth: true } },
+  { path: '/print-label', name: 'PrintLabel', component: PrintLabel, meta: { requiresAuth: true } },
+  { path: '/not-authorized', name: 'not-authorized', component: () => import('@/views/pages/maintenance/error/Error401Page.vue') }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  const isLoggedIn = userStore.isLoggedIn
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    next('/not-authorized')
+  } else {
+    next()
+  }
 })
 
 export default router
