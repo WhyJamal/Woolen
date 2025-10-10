d0f088eb0ab18ed9ca36d15cf452aa2e7b6ec525
Thanos-in <sayyodbee2006@gmail.com>
2025-10-10 16:58:51 +0500

    style: change modal button color from red to blue

--- src/views/pages/LoginPage.vue
+++ src/views/pages/LoginPage.vue
@@ -149,7 +149,7 @@
               @click="errorAuthentication = false"
               data-modal-hide="popup-modal"
               type="button"
-              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+              class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
             >
               Ок
             </button>
