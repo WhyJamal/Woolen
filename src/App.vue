1d428498b94c6a8c7b1b0f7877f86208d088c2f2
Thanos-in <sayyodbee2006@gmail.com>
2025-10-29 11:41:39 +0500

    fix(router): remove blank page issue on route change and preserve fade transition effect

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
-<template>
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" :key="$route.fullPath" />
-    </transition>
-  </router-view>
-</template>
-
 <script setup>
 // empty
 </script>
 
-<style>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 220ms ease;
+<template>
+  <transition name="fade" mode="out-in">
+    <router-view />
+  </transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
 }
-.fade-enter-from, .fade-leave-to { opacity: 0; }
-.fade-enter-to, .fade-leave-from { opacity: 1; }
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
