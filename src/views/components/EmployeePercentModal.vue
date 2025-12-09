8432db0f046ee0a2c37f7cc5dfb35aa8eda55ab1
Thanos-in <sayyodbee2006@gmail.com>
2025-12-09 12:21:41 +0500

    changes

--- src/views/components/EmployeePercentModal.vue
+++ src/views/components/EmployeePercentModal.vue
@@ -1,150 +1,84 @@
 <template>
-  <div
-    v-if="isOpen"
-    class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-  >
-    <div
-      class="bg-white rounded-lg shadow-xl w-full h-full max-w-5xl mx-4 max-h-[90vh] flex flex-col"
-    >
-      <div
-        class="flex justify-between items-center px-6 rounded-md py-4 border-b bg-white"
-      >
+  <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div class="bg-white rounded-lg shadow-xl w-full h-full max-w-5xl mx-4 max-h-[90vh] flex flex-col">
+      <div class="flex justify-between items-center px-6 rounded-md py-4 border-b bg-white">
         <h3 class="text-xl font-semibold text-gray-800">Employees percent</h3>
-        <button
-          @click="closeModal"
-          class="text-gray-400 hover:text-gray-600 transition-colors"
-        >
-          <svg
-            class="w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            ></path>
-          </svg>
-        </button>
+
+        <div class="flex gap-6">
+          <div class="flex gap-2">
+            <h3 class="text-sm font-semibold text-gray-800">Total:</h3>
+            <span class="text-sm font-semibold text-blue-800">{{ props.quantity }}</span>
+          </div>
+
+          <button @click="closeModal" class="text-gray-400 hover:text-gray-600 transition-colors">
+            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+          </button>
+        </div>
       </div>
 
       <div class="p-4 overflow-y-auto flex-1">
         <table class="w-full border-collapse bg-white">
           <thead>
             <tr class="bg-gray-100 border-b border-gray-300">
-              <th
-                class="px-3 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider"
-              >
+              <th class="px-3 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
                 Сотрудник
               </th>
-              <th
-                class="px-3 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider"
-              >
+              <th class="px-3 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
                 Действия
               </th>
-              <th
-                class="px-3 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider"
-              >
+              <th class="px-3 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
                 Метраж
               </th>
-              <th
-                class="px-3 py-3 text-center text-xs font-medium text-gray-600 uppercase tracking-wider"
-              >
+              <th class="px-3 py-3 text-center text-xs font-medium text-gray-600 uppercase tracking-wider">
                 Этап
               </th>
-              <th
-                class="px-3 py-3 text-center text-xs font-medium text-gray-600 uppercase tracking-wider"
-              >
+              <th class="px-3 py-3 text-center text-xs font-medium text-gray-600 uppercase tracking-wider">
                 Сложность
               </th>
-              <th
-                class="px-3 py-3 text-center text-xs font-medium text-gray-600 uppercase tracking-wider"
-              >
+              <th class="px-3 py-3 text-center text-xs font-medium text-gray-600 uppercase tracking-wider">
                 Смена
               </th>
             </tr>
           </thead>
           <tbody class="bg-white">
-            <tr
-              v-for="(employee, index) in employees"
-              :key="index"
-              class="border-b border-gray-200 hover:bg-gray-50"
-            >
+            <tr v-for="(employee, index) in employees" :key="index" class="border-b border-gray-200 hover:bg-gray-50">
               <td class="px-3 py-2.5">
                 <div class="flex items-center gap-1">
-                  <Listbox
-                    :modelValue="employee"
-                    @update:modelValue="(val) => updateEmployee(index, val)"
-                    as="div"
-                    class="flex-1"
-                  >
+                  <Listbox :modelValue="employee" @update:modelValue="(val) => updateEmployee(index, val)" as="div"
+                    class="flex-1">
                     <div class="relative overflow-visible">
                       <ListboxButton
-                        class="relative w-full bg-gray-50 text-gray-700 text-sm rounded px-2 py-1.5 h-9 overflow-hidden text-left focus:outline-none focus:ring-1 focus:ring-blue-400 pr-8"
-                      >
+                        class="relative w-full bg-gray-50 text-gray-700 text-sm rounded px-2 py-1.5 h-9 overflow-hidden text-left focus:outline-none focus:ring-1 focus:ring-blue-400 pr-8">
                         <span class="block truncate">{{
                           employee.name || "Выберите сотрудника"
                         }}</span>
-                        <span
-                          class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
-                        >
-                          <svg
-                            class="h-5 w-5 text-gray-400"
-                            viewBox="0 0 20 20"
-                            fill="none"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M6 8l4 4 4-4"
-                            />
+                        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="none" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4" />
                           </svg>
                         </span>
                       </ListboxButton>
-                      <transition
-                        leave-active-class="transition duration-100 ease-in"
-                        leave-from-class="opacity-100"
-                        leave-to-class="opacity-0"
-                      >
+                      <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
+                        leave-to-class="opacity-0">
                         <ListboxOptions
-                          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg focus:outline-none"
-                        >
-                          <ListboxOption
-                            v-for="emp in availableEmployees"
-                            :key="emp.GUID"
-                            :value="emp"
-                            v-slot="{ active, selected }"
-                            class="cursor-pointer"
-                          >
-                            <div
-                              :class="[ 
-                                active ? 'bg-blue-100 text-blue-900' : 'text-gray-900',
-                                'relative cursor-pointer select-none py-2 pl-3 pr-9'
-                              ]"
-                            >
-                              <span
-                                :class="[ selected ? 'font-semibold' : 'font-normal', 'block truncate' ]"
-                              >
+                          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg focus:outline-none">
+                          <ListboxOption v-for="emp in availableEmployees" :key="emp.GUID" :value="emp"
+                            v-slot="{ active, selected }" class="cursor-pointer">
+                            <div :class="[
+                              active ? 'bg-blue-100 text-blue-900' : 'text-gray-900',
+                              'relative cursor-pointer select-none py-2 pl-3 pr-9'
+                            ]">
+                              <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">
                                 {{ emp.name }}
                               </span>
-                              <span
-                                v-if="selected"
-                                :class="[ active ? 'text-blue-600' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4' ]"
-                              >
-                                <svg
-                                  class="h-5 w-5"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                                >
-                                  <path
-                                    fill-rule="evenodd"
+                              <span v-if="selected"
+                                :class="[active ? 'text-blue-600' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                  <path fill-rule="evenodd"
                                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                    clip-rule="evenodd"
-                                  />
+                                    clip-rule="evenodd" />
                                 </svg>
                               </span>
                             </div>
@@ -153,11 +87,8 @@
                       </transition>
                     </div>
                   </Listbox>
-                  <button
-                    @click="clearEmployee(index)"
-                    class="text-gray-400 hover:text-red-500 text-lg leading-none px-1"
-                    title="Очистить"
-                  >
+                  <button @click="clearEmployee(index)"
+                    class="text-gray-400 hover:text-red-500 text-lg leading-none px-1" title="Очистить">
                     ×
                   </button>
                 </div>
@@ -168,23 +99,18 @@
                 }}</span>
               </td>
               <td class="px-3 py-2.5">
-                <input
-                  type="number"
-                  v-model.number="employee.percent"
-                  class="w-full px-2 py-1.5 text-sm rounded focus:outline-none focus:ring-1 focus:ring-blue-400"
-                  min="0"
-                  max="100"
-                  placeholder="0"
-                />
+                <input type="number" v-model.number="employee.percent"
+                  class="w-full px-2 py-1.5 text-sm rounded focus:outline-none focus:ring-1 focus:ring-blue-400" min="0"
+                  max="100" placeholder="0" />
               </td>
               <td class="px-3 py-2.5">
-                <span class="w-full px-2 py-1.5 truncate block">{{ employee.stage?.name }}</span>
+                <span class="w-full px-2 py-1.5 text-sm truncate block">{{ employee.stage.name }}</span>
               </td>
-              <td class="px-3 py-2.5">
-                <span class="w-full px-2 py-1.5 truncate block">{{ employee.level?.name || props.level?.name }}</span>
+              <td class="py-2.5 px-3">
+                <span class="w-full text-sm px-2 py-1.5 truncate block">{{ employee.level?.name || props.level?.name }}</span>
               </td>
-              <td class="px-3 py-2.5 text-center">
-                <span class="w-36 text-blue-600 focus:ring-blue-500">
+              <td class="px-2 py-2.5 text-center">
+                <span class="w-full text-sm text-blue-600 focus:ring-blue-500">
                   {{ employee.shift }}
                 </span>
               </td>
@@ -194,32 +120,20 @@
       </div>
 
       <div class="flex justify-end p-4 border-t space-x-2">
-        <Button
-          variant="secondary"
-          @click="cancelRows"
-        >
+        <Button variant="secondary" @click="cancelRows">
           Аннулировать
         </Button>
 
-        <Button
-          variant="secondary"
-          @click="addRow"
-        >
+        <Button variant="secondary" @click="addRow">
           Добавить
         </Button>
 
-        <Button
-          @click="saveChanges"
-        >
+        <Button @click="saveChanges">
           Сохранить
         </Button>
       </div>
     </div>
-    <WarningModal
-      v-if="showWarning"
-      :message="warningMessage"
-      @close="showWarning = false"
-    />
+    <WarningModal v-if="showWarning" :message="warningMessage" @close="showWarning = false" />
   </div>
 </template>
 
@@ -263,7 +177,8 @@ const addRow = () => {
     GUID: "",
     action: { name: "", code: "" },
     percent: 0,
-    stage: { name: userStore.user.stage, code: userStore.user.stage_code },
+    stage: { name: "", code: "" },
+    // stage: { name: userStore.user.stage, code: userStore.user.stage_code },
     shift: "",
     level: props.level || {},
   });
@@ -279,9 +194,10 @@ const updateEmployee = (index, selectedEmployee) => {
     GUID: selectedEmployee.GUID || "",
     action: selectedEmployee.action || { name: selectedEmployee.action?.name || "", code: selectedEmployee.action?.code || "" },
     percent: existingPercent,
-    stage: selectedEmployee.stage || { name: userStore.user.stage, code: userStore.user.stage_code },
+    stage: selectedEmployee.stage || { name: selectedEmployee.stage.name, code: selectedEmployee.stage.code },
+    // stage: selectedEmployee.stage || { name: userStore.user.stage, code: userStore.user.stage_code },
     shift: selectedEmployee.shift || "",
-    level: props.level || selectedEmployee.level || {},
+    level: props.level || {},
   };
 };
 
@@ -296,13 +212,7 @@ const fetchRowData = async () => {
       params: { stage: userStore.user.stage_code },
     });
 
-    const data = (response.data || []).map((item) => ({
-      ...item,
-      stage: {
-        name: userStore.user.stage,
-        code: userStore.user.stage_code,
-      },
-    }));
+    const data = response.data;
 
     if (Array.isArray(data)) {
       availableEmployees.value = data;
@@ -315,7 +225,7 @@ const fetchRowData = async () => {
 onMounted(async () => {
   try {
     await fetchRowData();
-  } catch (error) {}
+  } catch (error) { }
 });
 
 watch(
